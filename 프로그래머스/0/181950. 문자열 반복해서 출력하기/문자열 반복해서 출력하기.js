2016986ce4566fb51bd5966f93172ef1acbe569e
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2025-12-24 11:07:41 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 49.00 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.js
@@ -0,0 +1,9 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+}).on('line', function (line) {
+    [str, n] = line.split(' ');
+    
+    console.log(str.repeat(+n))
+});
