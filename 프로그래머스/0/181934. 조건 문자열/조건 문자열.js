60d6844549d70f9b0aafe1e00b070124390aa19c
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2025-12-27 06:35:14 +0900

    [level 0] Title: 조건 문자열, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -1,9 +1,19 @@
+const operation = {
+    ">=" : (n, m) => n >= m,
+    "<=" : (n, m) => n <= m,
+    ">!" : (n, m) => n > m,
+    "<!" : (n, m) => n < m
+}
+
 function solution(ineq, eq, n, m) {
-    return ineq === ">" && eq === "=" 
-        ? +(n >= m)
-        : ineq === "<" && eq === "="
-        ? +(n <= m)
-        : ineq === ">" && eq === "!"
-        ? +(n > m)
-        : +(n < m)
+    // return ineq === ">" && eq === "=" 
+    //     ? +(n >= m)
+    //     : ineq === "<" && eq === "="
+    //     ? +(n <= m)
+    //     : ineq === ">" && eq === "!"
+    //     ? +(n > m)
+    //     : +(n < m)
+    
+    const op = operation[`${ineq}${eq}`]
+    return +(op(n, m))
 }
