16117e9d38da4f44b1108d9d4e1fd89b0bb71728
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2025-12-26 10:59:23 +0900

    [level 0] Title: 문자열 섞기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,3 +1,18 @@
 function solution(str1, str2) {
-    return [...str1].reduce((acc, cur, i) => acc + cur + str2[i], "")
+    /**
+    * answer +=가 느려질 수 있음.(매번 새 문자열 생성)
+    */
+//     let answer = ""
+    
+//     for (let i = 0; i < str1.length; i++) {
+//         answer += str1[i] + str2[i]
+//     }
+    
+//     return answer
+    
+    // 방법 1
+    // return [...str1].reduce((acc, cur, i) => acc + cur + str2[i], "")
+    
+    // 방법 2
+    return Array.from(str1, (cur, i) => cur + str2[i]).join("")
 }
