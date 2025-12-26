ebcf1a96d82c800ed8b6478ed7d3ef65ff26447a
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2025-12-26 11:06:28 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,6 +1,8 @@
 function solution(a, b) {
-    const firstAnswer = Number(a.toString() + b.toString())
-    const secondAnswer = Number(b.toString() + a.toString())
+//     const firstAnswer = Number(a.toString() + b.toString())
+//     const secondAnswer = Number(b.toString() + a.toString())
     
-    return firstAnswer >= secondAnswer ? firstAnswer : secondAnswer
+//     return firstAnswer >= secondAnswer ? firstAnswer : secondAnswer
+    
+    return Math.max(`${a}${b}`, `${b}${a}`)
 }
