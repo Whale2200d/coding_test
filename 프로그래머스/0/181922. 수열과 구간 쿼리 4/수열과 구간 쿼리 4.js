ff11aa267b810677dbadd2fc8f9c59359cb48274
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2025-12-29 10:54:21 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 2.98 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,11 +1,8 @@
 function solution(arr, queries) {
-    const result = [...arr]
-    
-    queries.map(([s, e, k]) => {
+    return queries.reduce((acc, [s, e, k]) => {
         for (let i = s; i < e+1; i++) {
-            if (i % k === 0) result[i] += 1
+            if (i % k === 0) acc[i] += 1
         }
-    })
-    
-    return result
+        return acc
+    }, [...arr])
 }
