0d602e4eb6aec1eadab96a0eb56f5ce5578501b3
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2025-12-29 10:29:10 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 4.73 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,5 +1,3 @@
 function solution(arr, queries) { 
-    return queries.map((([s, e, k]) => arr.slice(s, e+1).reduce((best, v) => {
-        return v > k && (best === null || v < best) ? v : best
-    }, null) || -1))
+    return queries.map((([s, e, k]) => arr.slice(s, e+1).reduce((best, v) => v > k && (best === null || v < best) ? v : best, null) || -1))
 }
