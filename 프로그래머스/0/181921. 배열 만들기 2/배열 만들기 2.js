e85c27723748fa3abffda195f55dee1cc22f5b25
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2025-12-30 11:51:32 +0900

    [level 0] Title: 배열 만들기 2, Time: 154.17 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,11 +1,4 @@
 function solution(l, r) {
-    let answer = [];
-    
-    for (let i = l; i <= r; i++) {
-        const strArr = i.toString().split('')
-        
-        if (!(strArr.includes("1") || strArr.includes("2") || strArr.includes("3") || strArr.includes("4") || strArr.includes("6") || strArr.includes("7") || strArr.includes("8") || strArr.includes("9"))) answer.push(i)
-    }
-    
-    return answer.length === 0 ? [-1] : answer
+    const result = Array.from({ length:r-l+1 }, (_,i) => i+l).filter(n=>!/[^05]/.test(n));
+    return result.length ? result : [-1];
 }
