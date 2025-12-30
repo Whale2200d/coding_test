a1d7cd7c16a6474f917323a570395112aca84239
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2025-12-30 12:45:13 +0900

    [level 0] Title: 배열 만들기 4, Time: 4.42 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -3,21 +3,9 @@ function solution(arr) {
     let i = 0;
     
     while(i < arr.length) {
-        // if (stk.length === 0) { stk.push(arr[i]); i++; }
-        // else if (stk.length !== 0 && stk[stk.length-1] < arr[i]) { stk.push(arr[i]); i++; }
-        // else if (stk.length !== 0 && stk[stk.length-1] >= arr[i]) { stk.pop(); }
-        
-        if (stk.length === 0) {
-            stk.push(arr[i])
-            i++
-        } else {
-            if (stk[stk.length-1] < arr[i]) {
-                stk.push(arr[i])
-                i++
-            } else {
-                stk.pop()
-            }
-        }
+        if (stk.length === 0) { stk.push(arr[i]); i++; }
+        else if (stk.length !== 0 && stk[stk.length-1] < arr[i]) { stk.push(arr[i]); i++; }
+        else if (stk.length !== 0 && stk[stk.length-1] >= arr[i]) { stk.pop(); }
     }
     
     return stk;
