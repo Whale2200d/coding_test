7d4885491465c46c7bc0724f24481bf2301060f7
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2025-12-30 12:25:55 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -5,13 +5,9 @@ function solution(n) {
     while(result !== 1) {
         const even = result/2
         const odd = result*3+1
-        if (result % 2 === 0) {
-            arr.push(even)
-            result = even
-        } else {
-            arr.push(odd)
-            result = odd
-        }
+        
+        result % 2 === 0 ? arr.push(even) : arr.push(odd)
+        result = result % 2 === 0 ? even : odd
     }
     
     return arr;
