ee66fafb27d14cb4a21dcb5ab8ad159015d52456
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2025-12-31 05:46:12 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 3.03 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.js
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.js
@@ -1,5 +1,3 @@
 function solution(number) {
-    const result = number.split("").reduce((a, c) => a + Number(c), 0)
-    
-    return result % 9
+    return number.split("").reduce((a, c) => a + Number(c), 0) % 9
 }
