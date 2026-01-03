47fa227dd7f41649321f4313a4d6e569c300aecd
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-03 09:08:42 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,18 +1,14 @@
 function solution(my_string) {
-    const answer = []
+    const answer = Array(52).fill(0)
     
-    for (let code=65; code <= 90; code++) {
-        const charCode = String.fromCharCode(code)
-        const count = [...my_string].reduce((a, c) => c === charCode ? a+1 : a, 0)
+    for (const ch of my_string) {
+        const code = ch.charCodeAt(0)
         
-        answer.push(count)
-    }
-    
-    for (let code=97; code <= 122; code++) {
-        const charCode = String.fromCharCode(code)
-        const count = [...my_string].reduce((a, c) => c === charCode ? a+1 : a, 0)
-        
-        answer.push(count)
+        if (code >= 65 && code <= 90) {
+            answer[code - 65]++
+        } else if (code >= 97 && code <= 122) {
+            answer[26+(code-97)]++
+        }
     }
     
     return answer
