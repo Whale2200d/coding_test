4505dce2f50ecf0ccdf7d7261c5a43b93720be1b
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-04 07:00:56 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.22 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
@@ -1,5 +1,9 @@
 function solution(n, k) {
-    return Array.from({ length: n }, (_, i) => i+1).reduce((a, c) => {
-        if (c % k === 0) {a.push(c); return a;} else { return a; }
-    }, [])
+    const answer = [];
+    
+    for (let i=k; i <= n; i+=k) {
+        answer.push(i)
+    }
+    
+    return answer
 }
