6f0becaca66332683193556a44f20e2173a5a998
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-05 17:39:20 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,18 +1,7 @@
 function solution(str_list) {
-    const indexL = str_list.indexOf("l")
-    const indexR = str_list.indexOf("r")
-    
-    // l이나 r이 없다면 빈 리스트를
-    if (indexL === -1 && indexR === -1) return []
-    
-    if (indexL === -1) return str_list.slice(indexR+1)
-    
-    if (indexR === -1) return str_list.slice(0, indexL)
-    
-    // l과 r 중 먼저 나오는 문자열이 l이라면, 해당 문자열을 기준으로 왼쪽에 있는 문자열을 순서대로 담은 리스트를
-    if (indexL < indexR) return str_list.slice(0, indexL)
-    
-    // l과 r 중 먼저 나오는 문자열이 r이라면, 해당 문자열을 기준으로 오른쪽에 있는 문자열을 순서대로 담은 리스트를
-    return str_list.slice(indexR+1)
-    
+    for (let i=0; i <= str_list.length; i++) {
+        if (str_list[i] === "l") return str_list.slice(0, i)
+        if (str_list[i] === 'r') return str_list.slice(i+1)
+    }
+    return []
 }
