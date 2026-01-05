37dba6f28908c5f5686c9fdb0528120b5ca50293
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-05 17:13:08 +0900

    [level 0] Title: 배열 조각하기, Time: 2.12 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -1,6 +1,18 @@
-function solution(arr, query) {
-    return query.reduce((a, c, i) => {
-        if (i % 2 === 0) return a.slice(0, c+1)
-        return a.slice(c)
-    }, [...arr])
+// function solution(arr, query) {
+//     return query.reduce((a, c, i) => {
+//         if (i % 2 === 0) return a.slice(0, c+1)
+//         return a.slice(c)
+//     }, [...arr])
+// }
+
+function solution(arr, query, n=0) {
+    const isEven = !(n % 2)
+    const newArr = isEven ? arr.slice(0, query[n] + 1) : arr.slice(query[n])
+    
+    return n < query.length
+        ? solution(newArr, query, n+1)
+        : arr.length
+            ? arr
+            : [-1]
 }
+
