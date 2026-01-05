3524a4e11aea0b01a593dc6a48b2a77eefa1bee5
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-05 17:43:56 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,8 +1,3 @@
 function solution(num_list, n) {
-    return [...num_list].reduce((a, c, i) => {
-        if (i % n === 0) {
-            a.push(c)
-        }
-        return a
-    }, [])
+    return [...num_list].filter((v, i) => i % n === 0)
 }
