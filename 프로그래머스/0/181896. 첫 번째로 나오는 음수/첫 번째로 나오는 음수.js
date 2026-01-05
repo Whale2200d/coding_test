504de45e4ea047fe6af33751024720111caf17b2
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-05 16:06:01 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,7 +1,3 @@
 function solution(num_list) {
-    for (let i=0; i <= num_list.length; i++) {
-        if (Math.sign(num_list[i]) === -1) return i
-    }
-    
-    return -1
+    return num_list.findIndex((v) => v < 0)
 }
