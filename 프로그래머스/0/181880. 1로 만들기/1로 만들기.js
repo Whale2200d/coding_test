2893beba7531de3e9a358b53fee4aa45049433a6
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-06 17:26:58 +0900

    [level 0] Title: 1로 만들기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -1,14 +1,3 @@
 function solution(num_list) {
-    let arr = [...num_list]
-    let count = 0;
-    
-    while(!arr.every((v) => v === 1)) {
-        arr = arr.map((v) => {
-            if (v===1) return 1
-            count++
-            return v%2===0 ? v/2 : (v-1)/2
-        })
-    }
-    
-    return count
+    return num_list.map(v => v.toString(2).length - 1).reduce((a, c) => a + c);
 }
