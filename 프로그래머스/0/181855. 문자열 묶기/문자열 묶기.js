0379baabc65e6a011c03e563f0696c56054090eb
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-09 13:07:59 +0900

    [level 0] Title: 문자열 묶기, Time: 9.35 ms, Memory: 51.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -0,0 +1,10 @@
+function solution(strArr) {
+    const counts = []
+    
+    for (const str of strArr) {
+        const length = str.length
+        counts[length] = (counts[length] ?? 0) + 1
+    }
+    
+    return Math.max(...counts.filter(v => v !== undefined))
+}
