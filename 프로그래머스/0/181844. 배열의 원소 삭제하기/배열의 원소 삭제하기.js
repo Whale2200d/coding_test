bfa170a442720ac831f3a493b6d66cec6084b5e4
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-11 10:34:52 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -1,6 +1,3 @@
 function solution(arr, delete_list) {
-    return arr.reduce((a, c, i) => {
-        if (!delete_list.includes(c)) a.push(c)
-        return a
-    }, [])
+    return arr.filter((v) => !delete_list.includes(v))
 }
