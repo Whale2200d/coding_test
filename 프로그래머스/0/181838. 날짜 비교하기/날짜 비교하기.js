6789b78ffe0955fc1008ac97464240287b91af42
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-11 10:54:40 +0900

    [level 0] Title: 날짜 비교하기, Time: 4.39 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,13 +1,3 @@
 function solution(date1, date2) {
-    const [y1, m1, d1] = date1
-    const [y2, m2, d2] = date2
-    
-    if (y1 < y2) return 1
-    else if (y1 === y2) {
-        if (m1 < m2) return 1
-        else if (m1 === m2) {
-            return d1 < d2 ? 1 : 0
-        }
-    }
-    return 0
+    return new Date(date1) < new Date(date2) ? 1 : 0
 }
