0fecc2302fe487fe737ea090c265bf1b0e76e8e8
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-12 09:31:16 +0900

    [level 0] Title: l로 만들기, Time: 6.12 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -1,9 +1,3 @@
 function solution(myString) {
-    const ascii = 'l'.charCodeAt(0)
-    
-    return [...myString].reduce((a, c) => {
-        const cur = c.charCodeAt(0)
-        
-        return cur < ascii ? a+'l' : a+c
-    }, "")
+    return myString.replace(/[a-k]/g, 'l')
 }
