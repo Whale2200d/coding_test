4871683e3be055f494d820465442510d174719d1
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-12 12:15:33 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -2,7 +2,8 @@ function solution(common) {
     const a = common[0]
     const d = common[2] - common[1]
     const r = common[2] / common[1]
+    const n = common.length-1
     
-    if (d === common[1] - common[0]) return a+d*common.length
-    return a*r**common.length
+    if (d === common[1] - common[0]) return a+d*(n+1)
+    return a*r**(n+1)
 }
