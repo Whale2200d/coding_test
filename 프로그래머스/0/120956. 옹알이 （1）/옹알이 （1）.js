0b43538335c557068e90bfeab974f0e6c3994d3e
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-12 12:07:45 +0900

    [level 0] Title: 옹알이 (1), Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,29 +1,6 @@
 function solution(babbling) {
     const says = ["aya", "ye", "woo", "ma"]
-    const perm = permutations(says)
+    const regex = /^(aya|ye|woo|ma)+$/
     
-    return babbling.reduce((a, c) => perm.includes(c) ? a+1 : a, 0)
-}
-
-function permutations(arr) {
-    const result = [];
-
-  function dfs(path, used) {
-    if (path.length > 0) {
-      result.push(path.join(""));
-    }
-    if (path.length === arr.length) return;
-
-    for (let i = 0; i < arr.length; i++) {
-      if (used[i]) continue;
-      used[i] = true;
-      path.push(arr[i]);
-      dfs(path, used);
-      path.pop();
-      used[i] = false;
-    }
-  }
-
-  dfs([], Array(arr.length).fill(false));
-  return result;
+    return babbling.reduce((a, c) => regex.test(c) ? a+1 : a, 0)
 }
