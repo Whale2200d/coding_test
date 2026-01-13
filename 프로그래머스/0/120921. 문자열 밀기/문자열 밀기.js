015497ffccdf5392f11e11f487e08627fd2881d3
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-13 17:11:41 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,17 +1,3 @@
 function solution(A, B) {
-    if (A === B) return 0
-    
-    const spread = [...A]
-    let count = 1
-    
-    while(count < spread.length) {
-        const last = spread.pop()
-        spread.unshift(last)
-        
-        if (spread.join('') === B) return count
-        
-        count++
-    }
-    
-    return -1
+    return (B+B).indexOf(A)
 }
