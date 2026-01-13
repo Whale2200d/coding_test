e07e087b2b124e4ce8b456bc8dec421a0d383022
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-13 18:02:22 +0900

    [level 0] Title: OX퀴즈, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -1,12 +1,12 @@
 function solution(quiz) {
-    return quiz.reduce((a, c) => {
-        const [calc, z] = c.split(' = ')
+    return quiz.map((v) => {
+        const [calc, z] = v.split(' = ')
         const isAdd = calc.includes('+')
         const [x, y] = calc.split(isAdd ? ' + ' : ' - ')
         
         let result = 0;
         isAdd ? result = (+x)+(+y) : result = (+x)-(+y)
         
-        return result === (+z) ? [...a, 'O'] : [...a, 'X']
-    }, [])
+        return result === (+z) ? 'O' : 'X'
+    })
 }
