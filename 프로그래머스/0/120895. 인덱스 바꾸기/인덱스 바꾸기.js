52834cf6a8ef2551ba5c1182c496ece4bb16fdd7
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-14 12:50:14 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.08 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,10 +1,8 @@
 function solution(my_string, num1, num2) {
-    const answer = [...my_string]
+    const answer = my_string.split('');
     
-    // 1. 임시 temp 사용
-    const temp = answer[num1]
-    answer[num1] = answer[num2]
-    answer[num2] = temp
+    // 2. 구조 분해 할당
+    [answer[num1], answer[num2]] = [answer[num2], answer[num1]]
     
     return answer.join('')
 }
