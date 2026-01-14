98a39cd53974e2ba071c5ae5b8236447103ad47d
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-14 12:44:28 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,6 +1,7 @@
 function solution(my_string, num1, num2) {
     const answer = [...my_string]
-        
+    
+    // 1. 임시 temp 사용
     const temp = answer[num1]
     answer[num1] = answer[num2]
     answer[num2] = temp
