27466c3538ebcd50ac1f7893689741ed46a30c29
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-14 12:35:14 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,17 +1,5 @@
 function solution(s) {
-    // find 방식 -> O(n**2)
-    // return [...s].reduce((a, c, i) => {
-    //     const found = a.find(v => v.value === c)
-    //     found ? found.count += 1 : a.push({ value:c, count:1 })
-    //     return a
-    // }, []).filter(v => v.count === 1).map(v => v.value).sort().join('')
-    
-    // map 방식 -> O(n)
-    const map = new Map();
-    
-    for (const ch of s) {
-        map.set(ch, (map.get(ch) ?? 0)+1)
-    }
-    
-    return [...map.entries()].filter(([_, count]) => count===1).map(([count])=>count).sort().join('')
+    let res = [];
+    for (let c of s) if (s.indexOf(c) === s.lastIndexOf(c)) res.push(c);
+    return res.sort().join('');
 }
