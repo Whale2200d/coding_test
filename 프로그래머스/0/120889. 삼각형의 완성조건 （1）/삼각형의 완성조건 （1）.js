40a57efd121e9dd2a2f8fdae930eba370d063834
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-15 13:07:25 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,12 +1,5 @@
 function solution(sides) {
-    const max = Math.max(...sides)
-    const rests = sides.filter(v => v !== max)
+    const [x, y, z] = sides.sort((a, b) => a-b)
     
-    if (rests.length === 2) {
-        return rests[0]+rests[1] > max ? 1 : 2
-    } else if (rests.length === 1) {
-        return rests[0]+max > max ? 1 : 2
-    } else {
-        return 1
-    }
+    return x+y > z ? 1 : 2
 }
