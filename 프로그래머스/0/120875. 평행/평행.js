8d8171e62b6b577b114393b513ff1adc8544a5d5
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-16 15:24:29 +0900

    [level 0] Title: 평행, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.js
+++ 프로그래머스/0/120875. 평행/평행.js
@@ -1,18 +1,19 @@
 function solution(dots) {
     const [dot1, dot2, dot3, dot4] = dots
-    
-    const grade1 = getGrade(dot1, dot2)
-    const grade2 = getGrade(dot2, dot3)
-    const grade3 = getGrade(dot3, dot4)
-    const grade4 = getGrade(dot4, dot1)
-    const grade5 = getGrade(dot1, dot3)
-    const grade6 = getGrade(dot2, dot4)
-    
-    return grade1 === grade3 ||
-        grade2 === grade4 ||
-        grade5 === grade6 ? 1 : 0
+
+    if (isParallel([dot1, dot2], [dot3, dot4])) return 1;
+    if (isParallel([dot1, dot3], [dot2, dot4])) return 1;
+    if (isParallel([dot1, dot4], [dot2, dot3])) return 1;
+    return 0;
 }
     
-function getGrade(arr1, arr2) {
-    return (arr1[1]-arr2[1]) / (arr1[0]-arr2[0])
+function isParallel([a, b], [c, d]) {
+    const dy1 = b[1] - a[1];
+    const dx1 = b[0] - a[0];
+    const dy2 = d[1] - c[1];
+    const dx2 = d[0] - c[0];
+
+    // 나눗셈일 경우, 소수점에서 오차로 인해 판별이 어려워질 수 있음.
+    // 곱셈 비교로, 수치 안정성 확보
+    return dy1 * dx2 === dy2 * dx1; 
 }
