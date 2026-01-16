92ec291bc6a825a29fcb695376c5b7be72da8833
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-16 11:21:32 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,21 +1,7 @@
 function solution(a, b) {
-    let den = b / gcd(a, b) // 기약분수로 나타낸 분모
-    
-    while (den % 2 === 0) den /= 2
-    while (den % 5 === 0) den /= 5
-    
+    let den = b
+    while (den%2 === 0) den /= 2
+    while (den%5 === 0) den /= 5
+    if (a % den === 0) den=1
     return den === 1 ? 1 : 2
-}
-
-function gcd(a, b) {
-    a = Math.abs(a)
-    b = Math.abs(b)
-    
-    while (b != 0) {
-        const r = a % b
-        a = b
-        b = r
-    }
-    
-    return a
 }
