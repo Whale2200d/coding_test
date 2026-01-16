11bb772a047aa3dee41e6b6e78502c9a4846e4cc
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-16 10:42:48 +0900

    [level 0] Title: 로그인 성공?, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,13 +1,6 @@
 function solution(id_pw, db) {
     const [id, pw] = id_pw
     
-    let i = 0
-    while(i < db.length) {
-        const [db_id, db_pw] = db[i]
-        
-        if (db_id === id) return db_pw === pw ? "login" : 'wrong pw'
-        i++
-    }
-    
-    return 'fail'
+    const mappedDb = new Map(db)
+    return mappedDb.has(id) ? mappedDb.get(id) === pw ? 'login' : 'wrong pw' : 'fail'
 }
