2606cadd132d9aa240bfe11e0ccfbea30ce845d5
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-19 11:38:35 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,7 +1,4 @@
 function solution(box, n) {
-    const long = Math.floor(box[0] / n)
-    const wide = Math.floor(box[1] / n)
-    const high = Math.floor(box[2] / n)
-    
-    return long*wide*high
+    const [width, length, height] = box
+    return Math.floor(width / n) * Math.floor(length / n) * Math.floor(height / n)
 }
