2176327f9b4a1664a0d7e07b9e81cf7430106899
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-19 11:30:48 +0900

    [level 0] Title: 합성수 찾기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -4,8 +4,13 @@ function solution(n) {
     
     const compositeList = Array(n+1).fill(false)
     
-    for (let d=2; d*d<=n; d++) {        
+    // 경계선이 루트n인 이유: 루트n보다 클 경우 약수가 될 수 없음.
+    for (let d=2; d*d<=n; d++) {
+        
+        // 어떤 수(p)가 false라면 p는 소수
         if (compositeList[d] === false) {
+            // 에라토스테네스의 체
+            // 이 때, p를 약수로 하는 모든 수(p*p, p*(p+1), ...)는 합성수(true) 처리
             let p = d
             while (d*p <= n) {
                 compositeList[d*p] = true
