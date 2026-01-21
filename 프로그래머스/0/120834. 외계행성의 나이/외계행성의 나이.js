c3eaa7f60ffc373402417da8ea077a94ab63b3ee
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-21 11:07:57 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,4 +1,4 @@
 function solution(age) {
-    const changed = { 0:'a', 1:'b', 2:'c', 3:'d', 4:'e', 5:'f', 6:'g', 7:'h', 8:'i', 9:'j' }
+    const changed = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' ]
     return [...age.toString()].reduce((a, c) => a+changed[c], '')
 }
