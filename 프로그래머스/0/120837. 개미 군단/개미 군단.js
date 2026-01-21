bccb66771103b19472b9bd13478d9ed3aa9a8451
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-21 10:21:16 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,33 +1,14 @@
 function solution(hp) {
-    const map = { g:5, s:3, w:1 }
     let count = 0
     let _hp = hp
     
-    while(_hp > 0) {
-        const gc = Math.floor(_hp/map['g'])
-        const gm = _hp%map['g']
-        
-        _hp = gm
-        
-        if (gc > 0) {
-            count += gc
-        } else {
-            const sc = Math.floor(_hp/map['s'])
-            const sm = _hp%map['s']
-           
-            _hp = sm
-            
-            if (sc > 0) {
-                count += sc
-            } else {
-                const wc = Math.floor(_hp/map['w'])
-                const wm = _hp%map['w']
-                
-                count += wc
-                _hp = 0
-            }
-        }
-    }
+    count += Math.floor(_hp/5)
+    _hp %= 5
+    
+    count += Math.floor(_hp/3)
+    _hp %= 3
+    
+    count += _hp
     
     return count
 }
