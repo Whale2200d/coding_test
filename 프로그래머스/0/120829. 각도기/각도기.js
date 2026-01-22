172bb52a912d0887f53d0a9b10765400e010cf54
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-22 12:42:12 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,6 +1,3 @@
 function solution(angle) {
-    if (angle === 90) return 2
-    if (angle === 180) return 4
-    if (angle > 0 && angle < 90) return 1
-    return 3
+    return [1, 90, 91, 180].filter(v => angle>=v).length
 }
