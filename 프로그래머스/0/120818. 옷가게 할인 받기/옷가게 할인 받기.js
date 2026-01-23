6ded3b966876b8129376af25e9c9c85c364d4513
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-23 10:01:55 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,6 +1,13 @@
 function solution(price) {
-    if (price >= 500000) return Math.floor(price*0.8)
-    if (price >= 300000) return Math.floor(price*0.9)
-    if (price >= 100000) return Math.floor(price*0.95)
+    const discounts = [
+        { standard: 500000, rate: 20 },
+        { standard: 300000, rate: 10 },
+        { standard: 100000, rate: 5 },
+    ]
+    
+    for(let i=0; i<discounts.length; i++) {
+        if (price >= discounts[i].standard)
+            return Math.floor(price*(1-discounts[i].rate/100))
+    }
     return price
 }
