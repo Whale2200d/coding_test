8ed9b82960f58979bb638bdfb84f1181105889fd
Whale2200 <71238020+Whale2200d@users.noreply.github.com>
2026-01-23 09:43:52 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 54.01 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    const n = Number(input[0])
+    let answer = ''
+    
+    for (let i=1; i<=n; i++) {
+        answer += '*'.repeat(i)
+        answer += '\n'
+    }
+    
+    console.log(answer.trim());
+});
