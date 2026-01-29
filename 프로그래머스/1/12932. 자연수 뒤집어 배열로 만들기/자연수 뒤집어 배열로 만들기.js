e9cf82c8495dc8781bf30c7d8b98d9dd3a987f9e
gyeong-rak.im(Aiden) <71238020+Whale2200d@users.noreply.github.com>
2026-01-29 11:33:41 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -1,3 +1,15 @@
 function solution(n) {
-    return [...(n+"")].reverse().map((v) => +v)
+    // 1. 가독성 기준 (O(N*N))
+    // return [...(n+"")].reverse().map((v) => +v)
+    
+    // 2. 효율성 기준 (O(N))
+    let num = n
+    const answer = []
+    
+    do {
+        answer.push(num%10)
+        num = Math.floor(num/10)
+    } while (num > 0);
+        
+    return answer
 }
