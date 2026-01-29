78936b0deea9247c5455d24026bba7cd5da0c6bd
gyeong-rak.im(Aiden) <71238020+Whale2200d@users.noreply.github.com>
2026-01-29 11:47:57 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,13 +1,26 @@
 function solution(a, b) {
-    if (a === b) return a
+    // 1. 반복문 사용
+//     if (a === b) return a
     
-    const low = (a>b) ? b : a
-    const high = (a>b) ? a : b
-    let answer = 0
+//     const low = a > b ? b : a
+//     const high = a > b ? a : b
+//     let answer = 0
     
-    for (let i=low; i<=high; i++) {
-        answer += i
-    }
+//     for (let i=low; i<=high; i++) {
+//         answer += i
+//     }
     
-    return answer
+//     return answer
+    
+    
+    // 반복문 없이, 등차수열의 합 공식
+    const low = a > b ? b : a
+    const high = a > b ? a : b
+    
+    const sumLow = low * (1+low) / 2
+    const sumHigh = high * (1+high) / 2
+    
+    return sumHigh - sumLow + low
+    
+    // 반복문 없이, 가우스 공식 사용
 }
