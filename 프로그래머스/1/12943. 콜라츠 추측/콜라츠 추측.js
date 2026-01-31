856f661701ff8ddb4409442b526cc8e33afe32af
GYEONG-RAK. IM (Aiden) <71238020+Whale2200d@users.noreply.github.com>
2026-01-31 21:06:49 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -4,12 +4,9 @@ function solution(num) {
     let count = 0
     let result = num
     
-    while(count <= 500) {
+    while(count < 500 && result !== 1) {
+        result = result%2 === 0 ? result / 2 : result*3 + 1
         count++
-
-        result%2 === 0 ? result /= 2 : result = result*3 + 1
-        
-        if (result === 1) break
     }
     
     return result === 1 ? count : -1
