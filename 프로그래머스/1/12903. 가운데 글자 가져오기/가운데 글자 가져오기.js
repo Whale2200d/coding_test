01930960e54177ec7a9d53a8dffbcfddefef6de4
GYEONG-RAK. IM (Aiden) <71238020+Whale2200d@users.noreply.github.com>
2026-01-31 21:24:52 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,7 @@
+function solution(s) {
+    const middleIndex = Math.floor(s.length/2)
+    
+    return s.length%2 === 0
+        ? s.slice(middleIndex-1, middleIndex+1)
+        : s[middleIndex]
+}
