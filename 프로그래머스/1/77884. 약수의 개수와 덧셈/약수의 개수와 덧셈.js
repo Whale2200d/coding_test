1404bfdcdc6d62bda4fdb27009ceb986a0c24761
GYEONG-RAK. IM (Aiden) <71238020+Whale2200d@users.noreply.github.com>
2026-02-01 17:13:48 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,24 +1,9 @@
 function solution(left, right) {
-    let answer = 0
-    
-    for (let i=left; i<=right; i++) {
-        const count = countAliqout(i)
-        
-        answer = count%2 === 0 ? answer+i : answer-i
-    }
-    
-    return answer
-}
-
-function countAliqout(num) {
-    const arr = []
-    
-    for (let i=1; i*i<=num; i++) {
-        if (num%i === 0) {
-            arr.push(i)
-            if (i === num/i) {continue} else { arr.push(num/i) }
-        }
-    }
-    
-    return arr.length
+	let answer = 0
+	
+	for (let i=left; i<=right; i++) {
+		answer = answer + (Number.isInteger(Math.sqrt(i)) ? -i : i)
+	}
+	
+	return answer
 }
