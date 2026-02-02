97b99d5e3d7c7d326eab5f97125b1ceea2fdd858
GYEONG-RAK. IM (Aiden) <71238020+Whale2200d@users.noreply.github.com>
2026-02-02 11:10:10 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 36.93 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    const result = []
+    
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i] === arr[i+1]) { continue }
+        else result.push(arr[i])
+    }
+    
+    return result
+}
