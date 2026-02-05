719427a17c5d1edd76930df7dd8a684793bd2bb1
GYEONG-RAK. IM (Aiden) <71238020+Whale2200d@users.noreply.github.com>
2026-02-05 11:19:29 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,14 +1,4 @@
 function solution(a, b, n) {
-    let emptyCount = n
-    let totalReceiveCount = 0
-    
-    while (emptyCount >= a) {
-        const exchangeCount = Math.floor(emptyCount / a)
-        const receiveCount = exchangeCount * b
-        
-        totalReceiveCount += receiveCount
-        emptyCount = (emptyCount % a) + receiveCount
-    }
-    
-    return totalReceiveCount
+    const service = (n-b) / (a-b)
+	return Math.floor(service) * b
 }
