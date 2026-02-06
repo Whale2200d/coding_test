920a21ad8fa38ced437e145f9fad785cef72f01c
GYEONG-RAK. IM (Aiden) <71238020+Whale2200d@users.noreply.github.com>
2026-02-06 13:02:03 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,5 @@
+function solution(n, arr1, arr2) {
+    const binary1 = arr1.map((v) => v.toString(2).padStart(n, '0'))
+    const binary2 = arr2.map((v) => v.toString(2).padStart(n, '0'))
+    return binary1.map((bi1, index) => [...bi1].map((b1, i) => b1 === '0' && binary2[index][i] === '0' ? ' ' : '#').join(''))
+}
