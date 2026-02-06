d82d5736c1a3951660b0f11c7c870eb85e4e5311
GYEONG-RAK. IM (Aiden) <71238020+Whale2200d@users.noreply.github.com>
2026-02-06 15:05:55 +0900

    [level 1] Title: 폰켓몬, Time: 0.72 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,7 +1,7 @@
 function solution(nums) {
     // 가질 수 있는 마리수: N/2
     const max = nums.length/2
-    const set = [...new Set(nums)]
+    const set = new Set(nums)
     
-    return set.length > max ? max : set.length
+    return set.size > max ? max : set.size
 }
