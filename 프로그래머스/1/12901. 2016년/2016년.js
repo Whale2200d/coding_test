b28875cff4f327d2000b2a4a3ce1078fb8fe4916
GYEONG-RAK. IM (Aiden) <71238020+Whale2200d@users.noreply.github.com>
2026-02-06 18:59:03 +0900

    [level 1] Title: 2016년, Time: 489.33 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,10 +1,15 @@
 function solution(month, day) {
-    const dayNames = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"]
-    const daysInMonth2016 = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    // 일반 방법
+//     const dayNames = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"]
+//     const daysInMonth2016 = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     
-    let offsetDays = 0    
-    for (let i=0; i<month-1; i++) offsetDays += daysInMonth2016[i]    
-    offsetDays += (day-1)
+//     let offsetDays = 0    
+//     for (let i=0; i<month-1; i++) offsetDays += daysInMonth2016[i]    
+//     offsetDays += (day-1)
     
-    return dayNames[offsetDays % 7]
+//     return dayNames[offsetDays % 7]
+    
+    // Date 방법
+    var tempDate = new Date(2016, month-1, day)
+    return tempDate.toString().slice(0,3).toUpperCase()
 }
