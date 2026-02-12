ae4e5585d76c4876d582617526a792872fc1c186
GYEONG-RAK. IM (Aiden) <71238020+Whale2200d@users.noreply.github.com>
2026-02-12 10:30:38 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.40 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,33 +1,34 @@
 function solution(survey, choices) {
-  const scoreByType = new Map() // 성격유형별 점수
-  const indicatorPairs = ["RT", "CF", "JM", "AN"]
+    // survey: 질문마다 판단하는 지표를 담은 문자열 배열
+    // choices: 검사자가 각 질문마다 선택한 선택지를 담은 정수 배열
+    // const standard = ["RT", "TR", "FC", "CF", "MJ", "JM", "AN", "NA"]
+    
+    /**
+    1. "AN"에는 5점 -> 약간 동의 -> 네오형에 1점
+    2. "CF"에는 3점 -> 약간 비동의 -> 콘형에 1점
+    3. "MJ"에는 2점 -> 비동의 -> 무지형에 2점
+    */
+    // 1. 점수 집계
+    let score = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 }
 
-  for (const pair of indicatorPairs) {
-    scoreByType.set(pair[0], 0)
-    scoreByType.set(pair[1], 0)
-  }
-
-  for (let questionIndex = 0; questionIndex < survey.length; questionIndex++) {
-    const [disagreeType, agreeType] = survey[questionIndex]
-    const choice = choices[questionIndex]
-
-    const distanceFromNeutral = Math.abs(choice - 4)
-    if (distanceFromNeutral === 0) continue
-
-    const earnedType = choice < 4 ? disagreeType : agreeType
-    scoreByType.set(earnedType, scoreByType.get(earnedType) + distanceFromNeutral)
-  }
-
-  let result = "";
-  for (const pair of indicatorPairs) {
-    const leftType = pair[0]
-    const rightType = pair[1]
-
-    const leftScore = scoreByType.get(leftType)
-    const rightScore = scoreByType.get(rightType)
-
-    result += leftScore >= rightScore ? leftType : rightType
-  }
-
-  return result
+    for (let i=0; i<survey.length; i++) {
+        const currSurvey = survey[i]
+        const currChoice = choices[i]
+        
+        if (currChoice === 4) continue;
+        
+        // 4보다 크면 오른쪽, 4보다 작으면 왼쪽 값에 점수 부여
+        const index = currChoice > 4 ? 1 : 0
+        score[currSurvey[index]] += Math.abs(currChoice - 4)
+    }
+    
+    // 2. 지표 내 비교
+    let typePairs = [ ['R', 'T'], ['C', 'F'], ['J', 'M'], ['A', 'N'] ]
+    let result = ''
+    
+    for (const [leftType, rightType] of typePairs) {
+        result += score[leftType] >= score[rightType] ? leftType : rightType
+    }
+    
+    return result
 }
