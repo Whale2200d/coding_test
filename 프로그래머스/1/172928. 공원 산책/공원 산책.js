d22b19d09df808e1990fe4aa437ae5639be20204
GYEONG-RAK. IM (Aiden) <71238020+Whale2200d@users.noreply.github.com>
2026-02-13 15:15:56 +0900

    [level 1] Title: 공원 산책, Time: 0.55 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -1,56 +1,52 @@
 function solution(park, routes) {
-  const height = park.length;
-  const width = park[0].length;
+	const height = park.length
+	const width = park[0].length
 
-  // 시작점 S 찾기
-  let startRow = 0;
-  let startColumn = 0;
-  for (let row = 0; row < height; row += 1) {
-    const column = park[row].indexOf("S");
-    if (column !== -1) {
-      startRow = row;
-      startColumn = column;
-      break;
-    }
-  }
+	// 0. 시작점 찾기
+	let startRow = 0
+	let startCol = 0
+	for (let row=0; row<height; row++) {
+		const col = park[row].indexOf('S')
+		if (col !== -1) { startRow=row; startCol=col; break; }
+	}
+	//////////////////
+	
+	const directionGuide = {
+		E: [0, 1], // 동
+		W: [0, -1], // 서
+		S: [1, 0], // 남
+		N: [-1, 0], // 북
+	}
+	
+	let currentRow = startRow
+	let currentCol = startCol
 
-  const directionDeltaByCode = {
-    N: [-1, 0], // 북
-    S: [1, 0],  // 남
-    W: [0, -1], // 서
-    E: [0, 1],  // 동
-  };
+	// 1. 경로 반복 시행	
+	for (const route of routes) {
+		const [direction, distanceText] = route.split(' ')
+		const [rowGuide, colGuide] = directionGuide[direction]
+		const distance = Number(distanceText)
+		
+		// 2. 경로 끝까지 가상이동 진행
+		let nextRow = currentRow
+		let nextCol = currentCol
+		let isBlocked = false
+		
+		for (let step=1; step<=distance; step++) {
+			nextRow += rowGuide
+			nextCol += colGuide
+			
+			const isOutOfBounds = nextRow < 0 || nextRow >= height || nextCol < 0 || nextCol >= width
+			
+			// 3. 막혔으면 break (공원을 벗어나거나, 장애물을 만나는지 하나라도 조건 충족하면)
+			if (isOutOfBounds || park[nextRow][nextCol] === 'X') { isBlocked=true; break; }
+		}
+	
+		// 4. 막혔으면 다음 명령 진행
+		if (isBlocked) continue;
+		currentRow=nextRow
+		currentCol=nextCol
+	}
 
-  let currentRow = startRow;
-  let currentColumn = startColumn;
-
-  for (const route of routes) {
-    const [directionCode, distanceText] = route.split(" ");
-    const distance = Number(distanceText);
-    const [rowDelta, columnDelta] = directionDeltaByCode[directionCode];
-
-    let nextRow = currentRow;
-    let nextColumn = currentColumn;
-    let isBlocked = false;
-
-    for (let step = 1; step <= distance; step += 1) {
-      nextRow += rowDelta;
-      nextColumn += columnDelta;
-
-      const isOutOfBounds =
-        nextRow < 0 || nextRow >= height || nextColumn < 0 || nextColumn >= width;
-
-      if (isOutOfBounds || park[nextRow][nextColumn] === "X") {
-        isBlocked = true;
-        break;
-      }
-    }
-
-    if (!isBlocked) {
-      currentRow = nextRow;
-      currentColumn = nextColumn;
-    }
-  }
-
-  return [currentRow, currentColumn];
+	return [currentRow, currentCol]
 }
