70861671c5316bdc9dedddb59f5b3e26dd3e6f46
GYEONG-RAK. IM (Aiden) <71238020+Whale2200d@users.noreply.github.com>
2026-02-13 16:08:19 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.72 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -0,0 +1,40 @@
+function solution(bandage, health, attacks) {
+    // bandage: [시전 시간, 초당 회복량, 추가 회복량]
+    // health: 최대 체력
+    // attacks: 몬스터의 [공격 시간, 피해량]을 담은 2차원 정수 배열 (오름차순 정렬 완료)
+    const [castTime, healPerSecond, bonusHeal] = bandage
+    const lastAttackTime = attacks[attacks.length-1][0]
+    
+    let currentHealth = health
+    let successSecond = 0 // 붕대 감기 연속 성공 시간
+    let attackIndex = 0
+    
+    // 1. 1 ~ 마지막 공격 시간까지 반복
+    for (let currentTime=1; currentTime<=lastAttackTime; currentTime++) {
+        const [attackTime, demage] = attacks[attackIndex]
+        const isAttack = attackIndex < attacks.length && attackTime === currentTime
+        
+        // 3. 공격 받아서 붕대 감기 실패
+        if (isAttack) {
+            currentHealth -= demage
+            if (currentHealth <= 0) return -1
+            
+            successSecond = 0
+            attackIndex++
+            continue;
+        }
+        
+        // 2. 붕대 감기 성공
+        successSecond++
+        currentHealth = Math.min(health, currentHealth+healPerSecond)
+        
+        // castTime까지 성공하면, 추가 체력 회복
+        if (castTime === successSecond) {
+            currentHealth = Math.min(health, currentHealth+bonusHeal)
+            successSecond = 0
+        }
+    }
+    
+    // 모든 공격이 끝난 직후 남은 체력 반환
+    return currentHealth
+}
