6da1e5ac49bda6652e88a915955360ef4039ab82
GYEONG-RAK. IM (Aiden) <71238020+Whale2200d@users.noreply.github.com>
2026-02-17 11:57:58 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.81 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,4 +1,6 @@
 function solution(A,B){
+    // 결과가 최소값이므로, 큰 수끼리 곱하면 손해
+    // 결과가 최소값이므로, 가장 큰 수는 가장 작은 수와 곱해야 함
     const sortedA = A.sort((a, b) => a-b)
     const sortedB = B.sort((a, b) => b-a)
     return sortedA.reduce((a, c, i) => a+(c*sortedB[i]), 0)
