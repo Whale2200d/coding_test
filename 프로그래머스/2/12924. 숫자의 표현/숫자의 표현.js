b6499a2454d62d0fa0b9a65371911622882965c4
GYEONG-RAK. IM (Aiden) <71238020+Whale2200d@users.noreply.github.com>
2026-02-20 12:02:37 +0900

    [level 2] Title: 숫자의 표현, Time: 0.17 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,16 +1,12 @@
 function solution(n) {
     let count = 0
-    
-    // 홀수 부분만 남기기 (홀수인 약수의 개수 = oddPart의 약수 개수)
-    let oddPart = n
-    while (oddPart%2 === 0) oddPart /= 2
-    
-    for (let d=1; d*d <= oddPart; d += 2) {
-				if (oddPart%d !== 0) continue // 약수가 아니면 다음으로 넘어가기
-				
-				count++
-				if (oddPart/d !== d) count++ // 제곱근이 아니면 짝이 되는 약수도 추가
+
+    for (let d = 1; d*d <= n; d++) {
+        if (n % d !== 0) continue
+
+        if (d % 2 === 1) count++
+        if (n/d !== d && n/d % 2 === 1) count++
     }
-    
+
     return count
 }
